db328e322b08d65d6e47026ea2a801ca4c484dbf
Reid Vandewiele <reid@puppetlabs.com>
2014-10-08 10:34:41 -0700

    Attempt to convert to a real mco client
    This is an initial effort commit; additional debugging likely required
    from here to get it to actually work.

--- mco-playbook.rb
+++ mco-playbook.rb
@@ -1,17 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'mcollective'
+
+include MCollective::RPC
 
 playbook = YAML.load(File.read(ARGV[0]))
 
 playbook.each do |set|
+  filter = set['filter']
   set['tasks'].each do |task|
-    params = task['parameters'].map {|key,value| key.to_s + '=' + value.to_s }
-    command = []
-    command << 'mco' << 'rpc'
-    command << task['agent'] << task['action']
-    command << params.join(' ')
-    command << set['filter']
-    puts command.join(' ')
+    mc = rpcclient(task['agent'])
+    mc.reset
+    mc.identity_filter filter['identity'] if filter['identity']
+    mc.class_filter filter['class'] if filter['class']
+    mc.compound_filter filter['compound'] if filter['compound']
+
+    parameters = task['parameters'].inject({}) do |result,(k,v)|
+      result[k.to_sym] = v
+      result
+    end
+
+    mc.method_missing(task['action'], parameters)
   end
 end
